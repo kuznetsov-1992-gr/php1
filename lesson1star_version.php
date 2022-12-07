0a23ee22dee8bed9797e23efe2623c7302a7cfcb
Aleksandr <skuznetsov92@mail.ru>
2022-12-07 15:27:21 +0300

    lesson_1.2

--- lesson1star_version.php
+++ lesson1star_version.php
@@ -3,13 +3,10 @@
 
     $content = file_get_contents("lesson1star_html.html");
 
+    $content = str_replace("{{title}}", $title, $content);
+
     echo($content);
 
-
-    //conclusion
-    //Warning: Unknown: failed to open stream: No such file or directory in Unknown on line 0
-
-    //Fatal error: Unknown: Failed opening required 'lesson1star_version.php' (include_path='.;C:\php\pear') in Unknown on line 0
 ?>
 
 
